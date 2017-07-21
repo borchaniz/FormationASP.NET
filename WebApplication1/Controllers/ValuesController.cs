3b3bf3b17e2150601bae848448bef8f4dd1a1c3c
Borchani yessine <borchaniz96@gmail.com>
2017-07-21 15:00:09 +0100

    aaa

--- WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/Controllers/ValuesController.cs
@@ -10,6 +10,7 @@ namespace WebApplication1.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
+
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
