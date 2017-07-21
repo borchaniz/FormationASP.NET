e878765b9118dfbc079b59411ac2c5d40dfd8fcf
Borchani yessine <borchaniz96@gmail.com>
2017-07-21 16:05:26 +0100

    dj:hfjdh

--- WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -5,7 +5,7 @@ namespace WebApplication1
 {
     public class BundleConfig
     {
-        //this is great a modification
+        //this is baaaaaaaad a modification
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
