3c138590871793ef9fa85331de06bbdeb843a431
Borchani yessine <borchaniz96@gmail.com>
2017-07-21 16:09:06 +0100

    shfbd,nbf

--- WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -5,7 +5,7 @@ namespace WebApplication1
 {
     public class BundleConfig
     {
-        //i saved
+        //i saved and it will work
         //this is baaaaaaaad a modification
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
