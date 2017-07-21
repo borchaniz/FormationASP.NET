e646c5862b66e1b539f93f97c8697f4c93947dc9
Borchani yessine <Borchani yessine>
2017-07-21 14:47:30 +0100

    Add project files.

---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models{
+    public class Product{
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Details { get; set; }
+        public String Price { get; set; }
+    }
+}
